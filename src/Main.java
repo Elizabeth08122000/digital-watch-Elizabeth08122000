a0944527e3bf3776a37b5d8a265fccdd82e07e75
Liza <bliza0812@gmail.com>
2020-05-19 15:26:58 +0400

    Answer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(n/60+":"+n%60);
+        in.close();
     }
 }
